7e45f1c1e3f78672f5a64d5be28e5aa282f1842c
Matthew F Leader <mleader@redhat.com>
2023-12-18 17:19:16 -0500

    change main

--- arcaflow_plugin_fio/__main__.py
+++ arcaflow_plugin_fio/__main__.py
@@ -2,5 +2,11 @@ import sys
 from arcaflow_plugin_sdk import plugin
 import fio_plugin
 
+print('sys executable')
+print(sys.executable)
+print('=================')
+print('sys modules')
+print(sys.modules)
+
 if __name__ == "__main__":
     sys.exit(plugin.run(plugin.build_schema(fio_plugin.run)))
