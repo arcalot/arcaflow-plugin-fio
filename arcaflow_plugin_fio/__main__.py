d9f243867b54de4b0ec6753c041ee32cf6dbd0bc
Matthew F Leader <mleader@redhat.com>
2023-12-18 19:07:26 -0500

    clean up __main__

--- arcaflow_plugin_fio/__main__.py
+++ arcaflow_plugin_fio/__main__.py
@@ -6,13 +6,6 @@ from arcaflow_plugin_fio import main
 
 if __name__ == "__main__":
     main.main()
-    # print('=================')
-    # print('sys modules')
-    # for key in sys.modules:
-    #     print(key)
-    # # print(sys.modules['builtin'])
-    # print(sys.modules['arcaflow_plugin_fio'])
-    # print(sys.modules['arcaflow_plugin_fio.fio_plugin'])
     # sys.exit(plugin.run(plugin.build_schema(fio_plugin.run)))
 
 
